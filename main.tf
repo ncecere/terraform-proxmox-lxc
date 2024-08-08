3a1c53666c0ce638b8f7d372a17298083e1eff9b
Nicholas Cecere <nicholas.cecere@ufl.edu>
2024-08-08 10:56:41 -0400

    finish first pass at funtionality

--- main.tf
+++ main.tf
@@ -3,7 +3,27 @@
 /* -------------------------------------------------------------------------- */
 
 locals {
-  selected_node = one(random_shuffle.node.result)
+  # Default configurations
+  default_network_interface = {
+    bridge = "vmbr0"
+    # enabled     = true
+    # firewall    = false
+    # mac_address = null
+    # mtu         = null
+    # name        = "veth"
+    # rate_limit  = null
+    # vlan_id     = null
+  }
+
+  swap_size = (
+    var.memory_mb <= 2048 ? var.memory_mb * 2 :
+    var.memory_mb <= 4096 ? var.memory_mb :
+    var.memory_mb <= 16384 ? var.memory_mb / 2 :
+    4096
+  )
+
+  network_interfaces = [for network_interface in var.network_interface : merge(local.default_network_interface, network_interface)]
+  selected_node      = one(random_shuffle.node.result)
 }
 
 /* -------------------------------------------------------------------------- */
@@ -19,4 +39,118 @@ resource "random_shuffle" "node" {
   result_count = 1
 }
 
+/* -------------------------------------------------------------------------- */
+/*                              SECRETS SELECTION                             */
+/* -------------------------------------------------------------------------- */
 
+resource "random_password" "create" {
+  count            = var.create_root_password == true ? 1 : 0
+  length           = 32
+  override_special = "_%@"
+  special          = true
+}
+
+resource "tls_private_key" "create" {
+  count     = var.create_ssh_keys == true ? 1 : 0
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                 LXC RESOURCE                               */
+/* -------------------------------------------------------------------------- */
+
+resource "proxmox_virtual_environment_container" "lxc" {
+  name        = var.hostname
+  description = var.description
+  tags        = local.normalized_tags
+
+  node_name = local.selected_node
+
+  startup {
+    order      = var.startup_config.order
+    up_delay   = var.startup_config.up_delay
+    down_delay = var.startup_config.down_delay
+  }
+
+  cpu {
+    architecture = var.cpu_architecture
+    cores        = var.cpu_cores
+    units        = var.cpu_units
+  }
+
+  memory {
+    dedicated = var.memory_mb
+    swap      = var.memory_swap_mb != null ? var.memory_swap_mb : local.swap_size
+  }
+
+  operating_system {
+    template_file_id = "${var.template_datastore}:vztmpl/${var.template_name}"
+    type             = var.template_type
+
+  }
+
+  # DATA disks
+  dynamic "disk" {
+    for_each = var.disks
+    iterator = "disk"
+    content {
+      datastore_id = disk.value.datastore_id
+      size         = disk.value.size
+    }
+  }
+
+  dynamic "network_interface" {
+    for_each = { for idx, network_interface in local.network_interfaces : idx => network_interface }
+    content {
+      bridge = network_interface.value.bridge
+      name   = "veth${network_interface.key}"
+    }
+  }
+
+  initialization {
+    # DNS configuration
+    dns {
+      domain  = var.dns_domain
+      servers = var.dns_servers
+    }
+
+    # IP configuration
+    ip_config {
+      dynamic "ipv4" {
+        for_each = var.ip_addresses != null ? var.ip_addresses : []
+        content {
+          address = ipv4.value.address
+          gateway = ipv4.value.gateway
+        }
+      }
+
+      dynamic "ipv4" {
+        for_each = var.ip_addresses == null ? [1] : []
+        content {
+          address = "dhcp"
+        }
+      }
+
+      user_account {
+        keys     = var.user_keys != null ? var.user : [trimspace(tls_private_key.create.public_key_openssh)]
+        password = var.user_password != null ? var.user_password : random_password.create.result
+      }
+
+    }
+  }
+}
+
+output "container_password" {
+  value     = random_password.create.result
+  sensitive = true
+}
+
+output "container_private_key" {
+  value     = tls_private_key.create.private_key_pem
+  sensitive = true
+}
+
+output "container_public_key" {
+  value = tls_private_key.create.public_key_openssh
+}
---
+++ variables.tf
@@ -0,0 +1,168 @@
+/* -------------------------------------------------------------------------- */
+/*                            ENVIRONMENT VARIABLES                           */
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------------------------------------------------- */
+/*                             REQUIRED VARIABLES                             */
+/* -------------------------------------------------------------------------- */
+
+# Basic VM Configuration
+variable "hostname" {
+  type        = string
+  description = "Virtual machine hostname."
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9-]{1,63}$", var.hostname))
+    error_message = "The hostname must be a valid DNS hostname, containing only alphanumeric characters and hyphens, and be no longer than 63 characters."
+  }
+}
+
+variable "description" {
+  type        = string
+  description = "Description of the virtual machine being created."
+
+  validation {
+    condition     = length(var.description) > 0 && length(var.description) <= 255
+    error_message = "The description must not be empty and should be no longer than 255 characters."
+  }
+}
+
+variable "tags" {
+  type        = list(string)
+  description = "A list of tags to apply to the LXC container. The 'terraform' tag will be automatically added."
+
+  validation {
+    condition     = length(var.tags) > 0
+    error_message = "At least one tag must be provided."
+  }
+
+  validation {
+    condition     = alltrue([for tag in var.tags : can(regex("^[a-zA-Z0-9-_]+$", tag))])
+    error_message = "Tags must only contain alphanumeric characters, hyphens, and underscores."
+  }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                             OPTIONAL VARIABLES                             */
+/* -------------------------------------------------------------------------- */
+
+# Startup Configuration
+variable "startup_config" {
+  type = object({
+    order      = string
+    up_delay   = string
+    down_delay = string
+  })
+  default = {
+    order      = "3"
+    up_delay   = "60"
+    down_delay = "60"
+  }
+  description = "Startup configuration for the VM."
+}
+
+# CPU Configuration
+variable "cpu_cores" {
+  type        = number
+  default     = 1
+  description = "Number of CPU cores to assign to the VM"
+
+  validation {
+    condition     = var.cpu_cores > 0 && var.cpu_cores <= 8
+    error_message = "The number of CPU cores must be between 1 and 8."
+  }
+}
+
+variable "cpu_architecture" {
+  type        = string
+  default     = "amd64"
+  description = "The CPU architecture"
+
+  validation {
+    condition     = can(regex("^(amd64|arm64|armhf|i386)$", var.cpu_architecture))
+    error_message = "Invalid value for cpu_architecture, only allowed options are: 'amd64', 'arm64', 'armhf', 'i386'"
+  }
+}
+
+variable "cpu_units" {
+  type        = number
+  default     = 1024
+  description = "CPU weight for a container, must be between 8 and 50000. Number is relative to weights of all the other running containers."
+
+  validation {
+    condition = (
+      var.cpu_units >= 8 &&
+      var.cpu_units <= 50000
+    )
+    error_message = "Invalid value for cpu_units, must be between 8 and 50000."
+  }
+}
+
+variable "memory_mb" {
+  type        = number
+  default     = 512
+  description = "Amount of memory to assign the VM (in megabytes)"
+
+  validation {
+    condition     = var.memory_mb >= 512 && var.memory_mb <= 8192
+    error_message = "The amount of memory assigned must be between 512 and 8192 megabytes."
+  }
+}
+
+variable "memory_swap_mb" {
+  type        = number
+  default     = null
+  description = "The swap size in megabytes"
+
+}
+
+variable "disks" {
+  type = set(object({
+    datastore_id = string
+    size         = number
+  }))
+  default     = []
+  description = "The disk configuration for the LXC."
+}
+
+# Network Configuration
+variable "ip_addresses" {
+  type = set(object({
+    address = string
+    gateway = optional(string)
+  }))
+  default     = []
+  description = "The IPv4 configuration for each network interface"
+}
+
+variable "create_root_password" {
+  type        = bool
+  default     = false
+  description = "Create root password for LXC"
+}
+
+variable "create_ssh_keys" {
+  type        = bool
+  default     = false
+  description = "Create SSH keys for root user in LXC."
+}
+
+variable "user_keys" {
+  type        = list(string)
+  default     = []
+  description = "The SSH keys for the root account"
+  validation {
+    condition     = var.create_ssh_keys || length(var.user_keys) > 0
+    error_message = "At least one SSH key must be provided when create_ssh_keys is false."
+  }
+}
+
+variable "user_password" {
+  type        = string
+  default     = null
+  description = "The password for the root account"
+  validation {
+    condition     = var.create_root_password || (var.user_password != null && length(var.user_password) >= 16)
+    error_message = "A password of at least 16 characters must be provided when create_root_password is false."
+  }
+}
